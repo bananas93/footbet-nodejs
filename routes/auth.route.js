afd0fcc9401af10b552d2e49fe15ebf31b244387
David <amerovdavid@gmail.com>
2021-09-02 19:52:59 +0300

    change google auth route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -8,7 +8,7 @@ const router = Router();
 passport.use(new GoogleStrategy({
   clientID: '675019088327-o7m0q41l96r78innp6dcbep8gmu6hr2i.apps.googleusercontent.com',
   clientSecret: 'zjXRM1LCGyIPFouWFEw7OUIj',
-  callbackURL: 'https://footbet-client.herokuapp.com/api/auth/google/callback',
+  callbackURL: 'https://footbet.herokuapp.com/api/auth/google/callback',
 },
 (async (accessToken, refreshToken, profile, done) => {
   const { displayName, id, emails } = profile;
@@ -38,7 +38,7 @@ router.get('/callback', (req, res, next) => passport.authenticate('google', asyn
       path: '/',
     },
   );
-  return res.redirect('hhttps://footbet-client.herokuapp.com/');
+  return res.redirect('https://footbet.netlify.app/');
 })(req, res, next));
 
 module.exports = router;
