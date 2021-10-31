53911d8c6f248c4927aa1a2c0fe00717e5cb9fa6
David Amerov <a-david@ukr.net>
2021-10-31 20:18:00 +0200

    Added auth for android

---
+++ routes/authAndroid.route.js
@@ -0,0 +1,44 @@
+const { Router } = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const db = require('../models');
+
+const router = Router();
+
+passport.use(new GoogleStrategy({
+  clientID: '675019088327-vesc9vhv5cuvmtvjc7icrvot9632el11.apps.googleusercontent.com',
+  clientSecret: 'zjXRM1LCGyIPFouWFEw7OUIj',
+  callbackURL: 'https://footbet.site/api/auth/google/callback',
+},
+(async (accessToken, refreshToken, profile, done) => {
+  const { displayName, id, emails } = profile;
+  const user = await db.User.findOrCreate({
+    where: { googleId: id },
+    defaults: {
+      name: displayName,
+      googleId: id,
+      email: emails[0].value,
+    },
+  });
+  done(user);
+})));
+
+router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/callback', (req, res, next) => passport.authenticate('google', async (user, err) => {
+  if (err) {
+    return res.status(500).json({ error: err.message });
+  }
+  const token = await db.User.generateAuthToken(user[0].dataValues);
+  res.cookie(
+    'JWToken',
+    token,
+    {
+      expires: new Date(Date.now() + 10 * 604800000),
+      path: '/',
+    },
+  );
+  return res.redirect(`footbetapp://footbetapp?token=${token}`);
+})(req, res, next));
+
+module.exports = router;
