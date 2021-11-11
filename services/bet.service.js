53e403b03cb4396f697f68926c763db185b174a9
David Amerov <a-david@ukr.net>
2021-11-11 18:45:10 +0200

    Change mobile build

--- services/bet.service.js
+++ services/bet.service.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-param-reassign */
 const db = require('../models');
 const createOrUpdate = require('../helpers/createOrUpdate');
+const calcFunctions = require('../utils/calcPoints');
 
 const createOrUpdateBet = async (userId, homeBet, awayBet, matchId, tournamentId) => {
   const values = {
@@ -18,27 +20,73 @@ const createOrUpdateBet = async (userId, homeBet, awayBet, matchId, tournamentId
 };
 
 const findAllUserBets = async (id, tournament) => {
-  const result = await db.Bet.findAll({
-    where: { userId: id, tournamentId: tournament },
-    attributes: ['id', 'homeBet', 'awayBet'],
-    include: [
-      {
-        model: db.Match,
-        attributes: ['id', 'stage', 'status', 'homeGoals', 'awayGoals', 'home_team', 'away_team'],
-        include: [{
+  try {
+    let matches = await db.Match.findAll({
+      attributes: ['id', 'stage', 'group', 'status', 'homeGoals', 'awayGoals', 'datetime', 'tournament_id'],
+      where: {
+        tournament_id: tournament,
+        status: ['Завершено', 'Live'],
+      },
+      include: [
+        {
           model: db.Team,
           as: 'homeTeam',
-          attributes: ['id', 'name'],
+          attributes: ['name'],
         },
         {
           model: db.Team,
           as: 'awayTeam',
-          attributes: ['id', 'name'],
-        }],
-      },
-    ],
-  });
-  return result;
+          attributes: ['name'],
+        },
+        {
+          model: db.Bet,
+          as: 'bets',
+          attributes: ['id', 'homeBet', 'awayBet'],
+          include: [
+            {
+              model: db.User,
+              as: 'user',
+              attributes: ['id', 'name'],
+              where: { id },
+            },
+          ],
+        },
+      ],
+    });
+    if (matches === null || matches.length < 1) {
+      return [];
+    }
+    matches = matches.map((game) => {
+      game.bets.map((bet) => {
+        const points = calcFunctions.calculate(
+          bet.homeBet,
+          bet.awayBet,
+          game.homeGoals,
+          game.awayGoals,
+        );
+        bet.dataValues.points = points.all;
+        return bet;
+      });
+      return game;
+    });
+    const groups = matches.reduce((allGroups, game) => {
+      const date = game.datetime.toISOString().split('T')[0];
+      if (!allGroups[date]) {
+        allGroups[date] = [];
+      }
+      allGroups[date].push(game);
+      return allGroups;
+    }, {});
+    const groupArrays = Object.keys(groups).map((date, index) => ({
+      id: index,
+      tour: groups[date][0].stage,
+      date,
+      games: groups[date],
+    }));
+    return groupArrays;
+  } catch (err) {
+    return err.message;
+  }
 };
 
 const deleteById = async (id) => db.Bet.destroy({ where: { id } });
