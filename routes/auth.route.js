0939b98ca7256b33d818919e38434315c0e9b6b3
David Amerov <davidam@varyence.com>
2023-09-26 15:02:08 +0300

    changed client build

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,44 +1,50 @@
-const { Router } = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const db = require('../models');
+const { Router } = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const db = require("../models");
 
 const router = Router();
 
-passport.use(new GoogleStrategy({
-  clientID: '675019088327-o7m0q41l96r78innp6dcbep8gmu6hr2i.apps.googleusercontent.com',
-  clientSecret: 'zjXRM1LCGyIPFouWFEw7OUIj',
-  callbackURL: `${process.env.CLIENT_URL}/api/auth/google/callback`,
-},
-(async (accessToken, refreshToken, profile, done) => {
-  const { displayName, id, emails } = profile;
-  const user = await db.User.findOrCreate({
-    where: { googleId: id },
-    defaults: {
-      name: displayName,
-      googleId: id,
-      email: emails[0].value,
-    },
-  });
-  done(user);
-})));
-
-router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/callback', (req, res, next) => passport.authenticate('google', async (user, err) => {
-  if (err) {
-    return res.status(500).json({ error: err.message });
-  }
-  const token = await db.User.generateAuthToken(user[0].dataValues);
-  res.cookie(
-    'JWToken',
-    token,
+passport.use(
+  new GoogleStrategy(
     {
-      expires: new Date(Date.now() + 10 * 604800000),
-      path: '/',
+      clientID:
+        "675019088327-o7m0q41l96r78innp6dcbep8gmu6hr2i.apps.googleusercontent.com",
+      clientSecret: "zjXRM1LCGyIPFouWFEw7OUIj",
+      callbackURL: `${process.env.CLIENT_URL}/api/auth/google/callback`,
     },
-  );
-  return res.redirect(`${process.env.CLIENT_URL}/profile`);
-})(req, res, next));
+    async (accessToken, refreshToken, profile, done) => {
+      const { displayName, id, emails } = profile;
+      const user = await db.User.findOrCreate({
+        where: { googleId: id },
+        defaults: {
+          name: displayName,
+          googleId: id,
+          email: emails[0].value,
+        },
+      });
+      done(user);
+    }
+  )
+);
+
+router.get(
+  "/",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get("/callback", (req, res, next) =>
+  passport.authenticate("google", async (user, err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    const token = await db.User.generateAuthToken(user[0].dataValues);
+    res.cookie("JWToken", token, {
+      expires: new Date(Date.now() + 10 * 604800000),
+      path: "/",
+    });
+    return res.redirect(`${process.env.CLIENT_URL}/profile`);
+  })(req, res, next)
+);
 
 module.exports = router;
